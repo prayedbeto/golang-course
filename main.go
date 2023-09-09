07dfc250e047168ce7f32c04e88d19f51fa24341
Luis Vasquez <luis.guichol22@gmail.com>
2023-09-09 15:56:02 -0600

    Variables, funciones y funciones de control

--- main.go
+++ main.go
@@ -2,11 +2,34 @@ package main
 
 import (
 	"fmt"
-	"iusa-auto-deploy/variables"
+	// "iusa-auto-deploy/variables"
+	"runtime"
 )
 
 func main() {
-	fmt.Println("Hola mundo!")
+	// fmt.Println("Hola mundo!")
 
-	variables.MuestroEnteros()
+	// variables.MuestroEnteros()
+
+	// variables.RestoVariables()
+
+	// estado, texto := variables.ConvertirATexto(1588)
+	// fmt.Println(estado)
+	// fmt.Println(texto)
+
+	if os := runtime.GOOS; os == "linux" || os == "darwin" {
+		fmt.Println("Esto no es Windows, es", os)
+	} else {
+		fmt.Print("Esto es Windows ", os)
+	}
+
+	// Switch
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
