3840274733aae33d025393ee4e804043fc3b3878
Luis Vasquez <luis.guichol22@gmail.com>
2023-09-09 16:06:42 -0600

    Ejercicio 01

--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
+	"iusa-auto-deploy/ejercicios"
 	// "iusa-auto-deploy/variables"
-	"runtime"
+	// "runtime"
 )
 
 func main() {
@@ -17,19 +18,25 @@ func main() {
 	// fmt.Println(estado)
 	// fmt.Println(texto)
 
-	if os := runtime.GOOS; os == "linux" || os == "darwin" {
-		fmt.Println("Esto no es Windows, es", os)
-	} else {
-		fmt.Print("Esto es Windows ", os)
-	}
+	// if os := runtime.GOOS; os == "linux" || os == "darwin" {
+	// 	fmt.Println("Esto no es Windows, es", os)
+	// } else {
+	// 	fmt.Print("Esto es Windows ", os)
+	// }
 
 	// Switch
-	switch os := runtime.GOOS; os {
-	case "linux":
-		fmt.Println("Esto es Linux")
-	case "darwin":
-		fmt.Println("Esto es Darwin")
-	default:
-		fmt.Printf("%s \n", os)
-	}
+	// switch os := runtime.GOOS; os {
+	// case "linux":
+	// 	fmt.Println("Esto es Linux")
+	// case "darwin":
+	// 	fmt.Println("Esto es Darwin")
+	// default:
+	// 	fmt.Printf("%s \n", os)
+	// }
+
+	// Ejercicio 01
+	numero, texto := ejercicios.ConvNumerico("50")
+
+	fmt.Println(numero)
+	fmt.Println(texto)
 }
